d014e98d8baea139b634b63cb23bd27beef9d4d6
hpaliwal8 <hpaliwal@enphaseenergy.com>
2025-02-09 15:01:24 +0530

    Hero section addition

--- threejs-portfolio/src/constants/index.js
+++ threejs-portfolio/src/constants/index.js
@@ -15,12 +15,12 @@ export const navLinks = [
         href: '#work',
     },
     {
-        id: 3,
+        id: 4,
         name: 'Projects',
         href: '#projects',
     },
     {
-        id: 4,
+        id: 5,
         name: 'Contact',
         href: '#contact',
     },
--- threejs-portfolio/src/sections/Hero.jsx
+++ threejs-portfolio/src/sections/Hero.jsx
@@ -1,4 +1,33 @@
+import { PerspectiveCamera, Ring } from "@react-three/drei"
+import HackerRoom from "../components/HackerRoom"
+import { Suspense } from "react"
+import CanvasLoader from "../components/CanvasLoader"
+import { Canvas } from "@react-three/fiber"
+import { useMediaQuery } from "react-responsive"
+
+import { calculateSizes } from "../constants"
+import Target from "../components/Target"
+import ReactLogo from "../components/ReactLogo"
+import Cube from "../components/Cube"
+import Rings from "../components/Rings"
+import HeroCamera from "../components/HeroCamera"
+import Button from "../components/Button"
+
 const Hero = () => {
+
+    const isSmall = useMediaQuery({
+        maxWidth: 440,
+    })
+    const isMobile = useMediaQuery({
+        maxWidth: 768
+    })
+    const isTablet = useMediaQuery({
+        minWidth: 768,
+        maxWidth: 1024,
+    })
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -7,9 +36,33 @@ const Hero = () => {
                 <p className="hero_tag text-gray_gradient">I build things for the web</p>
             </div>
             <div className="w-full h-full absolute inset-0">
-                {/**
-                    Three js code goes here
-                 */}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+                        <HeroCamera isMobile={isMobile}>
+                            <HackerRoom
+                                scale={sizes.deskScale}
+                                position={sizes.deskPosition}
+                                rotation={[0, 3, 0]}
+                            />
+                        </HeroCamera>
+                        <group>
+                            <Target position={sizes.targetPosition} />
+                            <ReactLogo position={sizes.reactLogoPosition} />
+                            <Cube position={sizes.cubePosition} />
+                            <Rings position={sizes.ringPosition} />
+                        </group>
+
+                        <ambientLight intensity={1} />
+                        <directionalLight intensity={0.5} position={[10, 10, 10]} />
+                    </Suspense>
+
+                </Canvas>
+            </div>
+            <div className="absolute bottom-7 left-0 right-0 w-full z-index-10 c-space">
+                <a href="#contact" className="w-fit">
+                    <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+                </a>
             </div>
         </section>
     )
